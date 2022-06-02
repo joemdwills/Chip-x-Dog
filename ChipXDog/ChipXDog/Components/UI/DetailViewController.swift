3f5ae51da3365f13ae42c53ae3c5df6d6a19822f
Joe Williams <joe.md.williams@gmail.com>
2022-06-02 15:38:15 +0100

    Add ListView and DetailView Controllers (UI)

---
+++ ChipXDog/ChipXDog/Components/UI/DetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.swift
+//  ChipXDog
+//
+//  Created by Joe on 02/06/2022.
+//
+
+import UIKit
+
+final class DetailViewController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ ChipXDog/ChipXDog/Components/UI/ListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChipXDog
+//
+//  Created by Joe on 28/05/2022.
+//
+
+import UIKit
+
+final class ListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
