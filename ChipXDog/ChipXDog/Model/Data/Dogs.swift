39d72332c2f235081d1d95e20b1f07f691b32dc0
Joe Williams <joe.md.williams@gmail.com>
2022-06-02 15:40:45 +0100

    Add Data for ListResponse into Model

---
+++ ChipXDog/ChipXDog/Model/Data/Dogs.swift
@@ -0,0 +1,13 @@
+//
+//  Dogs.swift
+//  ChipXDog
+//
+//  Created by Joe on 28/05/2022.
+//
+
+import Foundation
+
+struct ListResponse: Codable {
+    let message: Breeds
+    let status: String
+}
