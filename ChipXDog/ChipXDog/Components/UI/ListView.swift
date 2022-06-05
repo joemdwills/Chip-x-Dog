532ee7dac1a893fc8230dcc6cc4c0385d079762f
Joe Williams <joe.md.williams@gmail.com>
2022-06-05 13:32:41 +0100

    Refactor ListViewController to MVVM Architecture

---
+++ ChipXDog/ChipXDog/Components/UI/ListView.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  ChipXDog
+//
+//  Created by Joe on 28/05/2022.
+//
+
+import UIKit
+
+final class ListView: UITableViewController {
+    private var viewModel = ListViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Chip x Dog API"
+        viewModel.fetchDogList {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("----> num Dog Count: \(viewModel.dogs.count)")
+        return viewModel.dogs.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Dog", for: indexPath)
+        let dog = viewModel.dogs[indexPath.row]
+        if dog.subBreed == nil {
+            cell.textLabel?.text = dog.type.capitalized
+            cell.detailTextLabel?.text = ""
+            cell.accessibilityLabel = "Dog Breed, \(dog.type)"
+        } else {
+            cell.textLabel?.text = dog.type.capitalized
+            cell.detailTextLabel?.text = dog.subBreed!.capitalized
+            cell.accessibilityLabel = "Dog Breed, \(dog.subBreed! + dog.type)"
+        }
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let dvc = storyboard?.instantiateViewController(identifier: String(describing: DetailCollectionViewController.self)) as? DetailCollectionViewController {
+            dvc.dog = viewModel.dogs[indexPath.row]
+            navigationController?.pushViewController(dvc, animated: true)
+        }
+    }
+}
+
---
+++ ChipXDog/ChipXDog/Components/ViewModels/ListViewModel.swift
@@ -0,0 +1,57 @@
+//
+//  ListViewModel.swift
+//  ChipXDog
+//
+//  Created by Joe on 05/06/2022.
+//
+
+import Foundation
+import UIKit
+
+class ListViewModel {
+    var dogAPI = DogAPI()
+    var dogs = [Breed]()
+    
+    let decoder = JSONDecoder()
+    
+    func fetchDogList(completion: @escaping () -> (Void)) {
+        dogAPI.dogService.fetchDogList { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let data):
+                self.decodeResponse(data: data)
+            case .failure(let error):
+                // Present UIAlertController
+                print(error.localizedDescription)
+            }
+            completion()
+        }
+    }
+    
+    func decodeResponse(data: Data) {
+        do {
+            let list = try decoder.decode(Dogs.self, from: data)
+            print("----> Unaltered List: \(list)")
+            let mirrorList = Mirror(reflecting: list.message)
+            var breeds = [String]()
+            for (key, value) in mirrorList.children {
+                guard let key = key else { return }
+                breeds.append("\(key.capitalized)")
+                guard let value = value as? [Any] else { return }
+                if value.isEmpty {
+                    self.dogs.append(Breed(type: key))
+                } else {
+                    for subBreed in value {
+                        guard let subBreed = subBreed as? String else { return }
+                        self.dogs.append(Breed(type: key, subBreed: subBreed))
+                    }
+                }
+            }
+            print("----> Breeds Count: \(breeds.count)")
+            print("----> Sub-breeds Count: \(self.dogs.count)")
+        } catch {
+            // Present UIAlertController
+            print("Error Decoding")
+        }
+    }
+}
